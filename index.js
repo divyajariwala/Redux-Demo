0bcfdc937670ca69687d50ec2aa7a8ed1303fb4a
MyPapion <divyajariwala610@gmail.com>
2020-07-26 09:20:17 -0700

    Middleware Added

--- index.js
+++ index.js
@@ -2,9 +2,16 @@
 //initalize store
 const redux =require('redux')
 const createStore=redux.createStore
+const combinReducers=redux.combineReducers
+const applyMiddleware=redux.applyMiddleware
+//initalize middleware
+const reduxLogger=require('redux-logger')
+const logger=reduxLogger.createLogger
+
 
 //Create Action
 const buy_cake ='Buy_Cake'
+const buy_icecream ='Buy_icecream'
 
 //Create Action creater
 
@@ -14,15 +21,31 @@ function buycake(){
         info:"My first action function"
     }
 }
+function buyicecream(){
+    return{
+        type:buy_icecream,
+        info:"My seocund action function"
+    }
+}
 
 //Create Reducer
 
 //(prevstate,action)=>new state
-const initalstate={
+// const initalstate={
+//     numOfCakes:10
+// }
+//Splite the state
+
+const initalIcecreamState={
+    numOfIcecream:10
+}
+
+const initalCakeState={
     numOfCakes:10
 }
 
-const reducer=(state=initalstate,action)=>{
+//Reducer
+const cakeReducer=(state=initalCakeState,action)=>{
     switch(action.type){
         case buy_cake:return{
             numOfCakes : state.numOfCakes-1
@@ -31,14 +54,32 @@ const reducer=(state=initalstate,action)=>{
     }
 
 }
+const icecreamReducer=(state=initalIcecreamState,action)=>{
+    switch(action.type){
+        case buy_icecream:return{
+            numOfIcecream : state.numOfIcecream-1
+        }
+        default:return state
+    }
 
+}
+//Define root reducer for combining both reducer
+const rootReducer=combinReducers({
+    cake:cakeReducer,
+    icecream:icecreamReducer
+})
 //Create Store
 
-const store=createStore(reducer)
+const store=createStore(rootReducer,applyMiddleware(logger))
 console.log("Initali state",store.getState())
-store.subscribe(()=>{console.log("Updated state",store.getState())})
+store.subscribe(()=>{})
 store.dispatch(buycake())
 store.dispatch(buycake())
 store.dispatch(buycake())
 store.dispatch(buycake())
+store.dispatch(buyicecream())
+store.dispatch(buyicecream())
+store.dispatch(buyicecream())
+store.dispatch(buyicecream())
+
  
